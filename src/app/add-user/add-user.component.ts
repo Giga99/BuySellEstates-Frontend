e25b311716a5b80d5665a73c0766b2e1ace37318
Igor Stevanovic <35437662+Giga99@users.noreply.github.com>
2021-06-20 14:07:09 +0200

    Added select image in add user

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../auth.service';
 import { Location } from '@angular/common';
 import { User } from '../models/User';
 import { UsersService } from '../users.service';
+import { FilesService } from '../files.service';
 
 @Component({
   selector: 'app-add-user',
@@ -19,31 +20,49 @@ export class AddUserComponent implements OnInit {
   city: string;
   country: string;
   userType: string;
-  agency: string;
+  image;
+  imagePreview;
 
   constructor(
     private authService: AuthService,
     private usersService: UsersService,
-    private location: Location
+    private location: Location,
+    private filesService: FilesService
   ) { }
 
   ngOnInit(): void {
   }
 
-  register() {
-    const user = new User(
-      this.firstname, this.lastname, this.username, this.password, this.email, this.city, this.country, ''
-    );
-    user.userType = this.userType;
-    user.agency = this.agency;
+  selectImage(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.image = file;
 
-    this.authService.register(user).subscribe(response => {
-      if (response['message'] == 'user added') {
-        this.usersService.answerUserRegistration(this.username, true).subscribe((response) => {
-          alert(response['message']);
-          this.location.back();
-        });
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (_event) => {
+        this.imagePreview = reader.result;
       }
+    }
+  }
+
+  register() {
+    this.filesService.uploadSingleFile(this.image).subscribe((response) => {
+      let path: string = response['path'];
+      const user = new User(
+        this.firstname, this.lastname, this.username, this.password, this.email, this.city, this.country, "../.." + path.substring(19).replace("\\", "/").replace("\\", "/").replace("\\", "/")
+      );
+      user.userType = this.userType;
+      user.agency = (this.userType == 'agent') ? 'agency1' : null;
+
+      this.authService.register(user).subscribe(response => {
+        if (response['message'] == 'user added') {
+          this.usersService.answerUserRegistration(this.username, true).subscribe((response) => {
+            alert(response['message']);
+            this.location.back();
+          });
+        }
+      });
     });
   }
 }
